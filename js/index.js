0dd3789a43a9adb384e70b64cc79580a73be7b1d
Catofes <trangent.tk@gmail.com>
2014-06-30 23:45:10 +0800

    update

--- js/index.js
+++ js/index.js
@@ -10,11 +10,15 @@ function login() {
 }
 
 function getprofilepic(params) {
+	can=document.createElement("canvas");
+	can.width=500;
+	can.height=500;
+	$("div#d_p").appendChild(can);
 	$.getJSON('https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + params.access_token, function(json) {
 				document.myPic=new Image();
 				document.myPic.src=json.picture;
 				document.myPic.onload = function(){
-					ctx = $("#c_p").getContext('2d');
+					ctx = can.getContext('2d');
 					ctx.drawImage(document.myPic,0,0,500,500);
 				}
 				});
